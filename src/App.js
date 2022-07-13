dce21815f1d7101b51fd21c226eaab3a853ffbd9
seongjin77 <kssj9056@gmail.com>
2022-07-13 16:14:10 +0900

    props 넘기기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import React, {useState, useEFFect} from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id:1,
+    author: '김성진',
+    content: '하이 1',
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id:2,
+    author: '홍길동',
+    content: '하이 2',
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id:3,
+    author: '차차차',
+    content: '하이 3',
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id:4,
+    author: '정순대',
+    content: '하이 4',
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+  {
+    id:5,
+    author: '박주황',
+    content: '하이 5',
+    emotion: 1,
+    created_date: new Date().getTime()
+  },
+]
+
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,21 @@
+import DiaryItem from './DiaryItem'
+// props의 {}를 써주는건 props가 객체로오는데 그 객체중에서 어느 하나만 찝어서 가져오겠다는 뜻.
+const DiaryList = ({ diaryList }) => {
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 리스트가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it}/> // props에 이름을 안적어주고 전달해도 되나보다?
+        ))}
+      </div>
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+    diaryList : [],
+};
+
+export default DiaryList;
