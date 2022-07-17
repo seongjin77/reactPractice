d12985c1107c86c1f9ff4d20ba29bed9f02cc5be
seongjin77 <kssj9056@gmail.com>
2022-07-17 23:57:26 +0900

    리액트 연습

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ import DiaryList from './DiaryList';
 
 function App() {
   const [data, setData] = useState([]);
-  const dataId = useRef();
+  const dataId = useRef(0);
 
   const onCreate = (author, content, emotion) => {
       const created_date = new Date().getTime();
@@ -59,10 +59,16 @@ function App() {
       setData([newItem, ...data]);
   };
 
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`)
+    const newDiaryList = data.filter((it)=> it.id !== targetId);
+    setData(newDiaryList);
+  };
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList diaryList={data}/>
+      <DiaryList onDelete={onDelete} diaryList={data}/>
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,11 +1,34 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) => {
+import {useState} from 'react';
+
+
+const DiaryItem = ({onDelete ,id, author, content, created_date, emotion,}) => {
+
+    const [isEdit, setIsEdit] = useState(false);
+    const toggleIsEdit = () => setIsEdit(!isEdit);
+
+    const [localContent,setLocalContent] = useState('');
+
+    const handleDelete = () => {
+        if(window.confirm(`${id}번째 아이디를 삭제하시겠습니까?`)){
+            onDelete(id)
+        }
+    };
+
     return (<div className="DiaryItem">
         <div className="info">
             <span>작성자 : {author} | 감정점수 : {emotion}</span> <br/>
             <span className="date">{new Date(created_date).toLocaleString()}</span> <br/>
-            <span className="content">{content}</span>
+            <div className="content">
+                {isEdit ?
+                <>
+                <textarea value={localContent} 
+                onChange={(e) => setLocalContent(e.target.value)} />
+                </> 
+                : <>{content}</> }
+            </div>
         </div>
-        
+        <button onClick={handleDelete}>삭제하기</button>
+        <button onClick={toggleIsEdit}>수정하기</button>
     </div>)
 };
 
