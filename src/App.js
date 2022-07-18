40ef3b4070c032b977291c8eeb8bd3d271b8bf00
seongjin77 <kssj9056@gmail.com>
2022-07-19 00:21:37 +0900

    API practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import Lifecycle from './Lifecycle';
+
+//https://jsonplaceholder.typicode.com/posts/1/comments
 
 // const dummyList = [
 //   {
@@ -46,6 +48,27 @@ import Lifecycle from './Lifecycle';
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+  const getData = async() => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/1/comments'
+      ).then((res)=> res.json());
+
+      const initData = res.slice(0,20).map((it)=>{
+        return {
+          author: it.email,
+          content: it.body,
+          emotion: Math.floor(Math.random()*5)+1,
+          created_date: new Date().getTime(),
+          id: dataId.current++,
+        }
+      })
+
+      setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  },[]);
 
   const onCreate = (author, content, emotion) => {
       const created_date = new Date().getTime();
